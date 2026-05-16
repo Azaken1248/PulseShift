3cf28af6226b6811514196dd530b261c056ccbda
Azaken1248 <suvkrm@gmail.com>
2026-05-16 14:16:40 +0530

    Create models for gamenotes and map data

---
+++ Assets/Scripts/Data/MapDataModels.cs
@@ -0,0 +1,28 @@
+using System;
+
+public enum NoteType : byte
+{
+    Tap = 0,
+    SlideStart = 1,
+    SlideTick = 2,
+    SlideEnd = 3,
+    Spinner = 4
+}
+
+[Serializable]
+public struct GameNote
+{
+    public double Timestamp;
+    public byte LaneIndex;
+    public NoteType Type;
+    public int SlideId;
+    public float SlideTargetX;
+}
+
+public class ConvertedMapData
+{
+    public GameNote[] Notes;
+    public int TotalNotes;
+
+    public double ApproachTime;
+}
